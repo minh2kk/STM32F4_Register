26a10eaada2a965b373528c216dd2d743e7b5473
Vu Van Minh <minhv3040@gmail.com>
2023-01-03 22:14:43 +0700

    Initial

--- Libraries/Inc/rcc.h
+++ Libraries/Inc/rcc.h
@@ -3,7 +3,7 @@
 
 #include "define.h"
 
-
+void RCC_Config(void);
 
 
 #endif
--- User/Inc/define.h
+++ User/Inc/define.h
@@ -3,7 +3,13 @@
 
 #pragma  once
 
-
+#define RCC_BASE_ADDRESS	((uint32_t)0x40023800)
+#define GPIOA_BASE_ADDRESS	((uint32_t)0x40020000)
+#define GPIOB_BASE_ADDRESS	((uint32_t)0x40020400)
+#define GPIOC_BASE_ADDRESS	((uint32_t)0x40020800)
+#define GPIOD_BASE_ADDRESS	((uint32_t)0x40020C00)
+#define GPIOE_BASE_ADDRESS	((uint32_t)0x40021000)
+#define GPIOF_BASE_ADDRESS	((uint32_t)0x40021400)
 
 
 #endif
